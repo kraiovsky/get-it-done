9bdfe976aef7213f28894034906836b0f8f3b547
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-08-04 23:05:10 +0200

    add got package and call api

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const readlineSync = require('readline-sync')
+const got = require('got')
+
+const callApi = async (tasks, troopers, troopersPerTask) => {
+  try {
+    const response = await got.post('https://get-it-done.rkrayovskyy.now.sh/api', {
+      body: {
+        tasks,
+        troopers,
+        troopersPerTask,
+      },
+      json: true,
+    })
+    console.log(response.body)
+  } catch (error) {
+    console.log(error.response.body)
+  }
+}
 
 const troopers = []
 const tasks = []
@@ -15,3 +32,7 @@ while (repeat) {
   input !== '' ? tasks.push(input) : (repeat = false)
 }
 console.log(tasks)
+
+const troopersPerTask = readlineSync.question('Troopers per task? ')
+
+callApi(tasks, troopers, troopersPerTask)
