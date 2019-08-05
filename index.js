6d486ecbc28a6adbf9a5b0bce905e271d62b9a39
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-08-05 20:20:38 +0200

    move things around for better file structure

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
-const readlineSync = require('readline-sync')
-const got = require('got')
+const buildProps = require('./lib/cli')
+const callApi = require('./lib/api')
 
-const callApi = async (tasks, troopers, troopersPerTask) => {
-  try {
-    const response = await got.post('https://get-it-done.rkrayovskyy.now.sh/api', {
-      body: {
-        tasks,
-        troopers,
-        troopersPerTask,
-      },
-      json: true,
-    })
-    console.log(response.body)
-  } catch (error) {
-    console.log(error.response.body)
-  }
-}
-
-const troopers = []
-const tasks = []
-let repeat = true
-
-while (repeat) {
-  const input = readlineSync.question('Troopers? ')
-  input !== '' ? troopers.push(input) : (repeat = false)
-}
-console.log(troopers)
-repeat = true
-while (repeat) {
-  const input = readlineSync.question('Tasks? ')
-  input !== '' ? tasks.push(input) : (repeat = false)
-}
-console.log(tasks)
-
-const troopersPerTask = readlineSync.question('Troopers per task? ')
-
+const { troopers, tasks, troopersPerTask } = buildProps()
 callApi(tasks, troopers, troopersPerTask)
