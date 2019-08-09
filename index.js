eed29f742c584c46ff5cf2c42530f9ec96ff735a
Roman Krayovskyy <rkrayovskyy@gmail.com>
2019-08-09 23:28:51 +0200

    try-catch cli execution

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const buildProps = require('./lib/cli')
 const callApi = require('./lib/api')
 
 const run = async () => {
-  const { troopers, tasks, troopersPerTask } = await buildProps()
-  await callApi(tasks, troopers, troopersPerTask)
+  try {
+    const { troopers, tasks, troopersPerTask } = await buildProps()
+    await callApi(tasks, troopers, troopersPerTask)
+  } catch {
+    console.error(`Not sure what's happenned. Here's a unicorn for you: 🦄`)
+  }
 }
 run()
